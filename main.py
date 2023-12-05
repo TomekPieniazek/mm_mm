71f78f2e0aaaf9359551b480d7b5922fb20d51a3
Gamenight2008 <u65_tompie_waw@technischools.com>
2023-12-05 12:41:41 +0100

    finish

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def print_sudoku(board):
 def possibilities_on_line(board, row, column):
     rows = []
     columns = []
-    # Print in row
+
     for i in range(len(board[row])):
         columns.append(board[row][i])
 
@@ -59,36 +59,33 @@ def change_number(row, column, number, board):
     return board
 
 
-def is_valid(board, row, col, num):
-    for x in range(9):
-        if board[row][x] == num:
-            return False
+def is_valid(board):
+    if 0 not in board[0] and 0 not in board[1] and 0 not in board[2] and 0 not in board[3] and 0 not in board[4] and 0 not in board[5] and 0 not in board[6] and 0 not in board[7] and 0 not in board[8]:
+        return True
+    else:
+        return False
 
-    for x in range(9):
-        if board[x][col] == num:
-            return False
-
-    start_row, start_col = row - row % 3, col - col % 3
-    for i in range(3):
-        for j in range(3):
-            if board[i + start_row][j + start_col] == num:
-                return False
-
-    return True
 
 
 def solve_sudoku(board):
+
+    if is_valid(board):
+        return board
+
     for i in range(9):
         for j in range(9):
             if board[i][j] == 0:
                 possibilities = possibilities_on_line(board, i, j)
                 box_possibilities = possibilities_in_box(board, i, j)
-                missing_numbers = sorted(list(set(range(10)) - set(possibilities) - set(box_possibilities)))
+                print(possibilities)
+                print(box_possibilities)
+                missing_numbers = [value for value in possibilities if value in box_possibilities]
+                print(missing_numbers)
+                print('---------------------------')
                 if len(missing_numbers) == 1:
                     board = change_number(i, j, missing_numbers[0], board)
-                print(missing_numbers)
-    return board
-    # return solve_sudoku(board)
+                    print('Board has been changed')
+    return solve_sudoku(board)
 
 
 print_sudoku(solve_sudoku(sudoku_board))
